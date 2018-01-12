0f91175351ebe1148083b078e31db7ddd52b4591
admin <vinicius.monteiro@processmind.com.br>
2018-01-12 16:20:33 -0200

    Subindo para o GITHUB

---
+++ database/migrations/2016_06_16_190458_table_pessoas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class TablePessoas extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('pessoas', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('nome');
+			$table->string('email');
+			$table->string('login');
+			$table->string('status');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('pessoas');
+	}
+}
